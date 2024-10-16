640fe008d0979187b6633393a9ead767d7689077
InfinityScripter <talalaev.misha@gmail.com>
2024-10-17 02:24:18 +0300

    add tailwind , add MUI , refactor to new style components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public title = 'univers-app-16';
+
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,51 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {UserProfileComponent} from "./components/user-profile/user-profile.component";
 import {MaskEmailPipe} from "./helpers/pipe/mask-email.pipe";
 import {HttpClientModule} from "@angular/common/http";
-import { DataListComponent } from './components/data-list/data-list.component';
+import {DataListComponent} from './components/data-list/data-list.component';
 import {FormsModule} from "@angular/forms";
-import { PostListComponent } from './components/post-list/post-list.component';
-import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { CalcComponent } from './components/calc/calc.component';
+import {PostListComponent} from './components/post-list/post-list.component';
+import {HomeComponent} from './pages/home/home.component';
+import {AboutComponent} from './pages/about/about.component';
+import {ContactComponent} from './pages/contact/contact.component';
+import {NavBarComponent} from './components/nav-bar/nav-bar.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from '@angular/material/icon';
+import {CalcComponent} from "./pages/calc/calc.component";
+import {MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from "@angular/material/select";
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        UserProfileComponent,
-        DataListComponent,
-        PostListComponent,
-        HomeComponent,
-        AboutComponent,
-        ContactComponent,
-        CalcComponent
-    ],
+  declarations: [
+    AppComponent,
+    UserProfileComponent,
+    DataListComponent,
+    PostListComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    CalcComponent,
+    NavBarComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MaskEmailPipe,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
