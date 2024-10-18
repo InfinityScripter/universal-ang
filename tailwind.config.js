c104a8efde53c8caae26f4e148015cba645bb675
InfinityScripter <talalaev.misha@gmail.com>
2024-10-18 23:26:10 +0300

    add new font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,13 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['JetBrains Mono', 'monospace'],
+      },
+    },
   },
+  variants: {},
   plugins: [],
-}
+};
 
